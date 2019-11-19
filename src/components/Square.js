1aacc94d2b9eea8ad6d785c695eabb3af804d77d
msukheja <meenakshi.sukheja@diamondresorts.com>
2019-11-19 15:16:45 +0530

    Added objects

---
+++ src/components/Square.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Square.css';
+
+function Square(props){
+    return(
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
+
+// class Square extends React.Component {
+//     render() {
+//         return (
+//             <button 
+//             className="square" 
+//             onClick={() => this.props.onClick()}>
+//                 {this.props.value}
+//             </button>
+//         );
+//     }
+// }
+
+export default Square;
